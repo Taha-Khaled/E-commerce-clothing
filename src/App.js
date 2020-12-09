1c0076443cafd6b9965cbbff06dc7292f27951ba
taha apps <appsbytaha@gmail.com>
2020-12-09 02:09:26 +0200

    update menuitem to navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const Hatpage = (props) =>(
+<div>
+  <h1>Hats Page</h1>
+  <button onClick = {()=> props.history.push('/')}>back</button>
+</div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={Hatpage}/>
     </div>
   );
 }
